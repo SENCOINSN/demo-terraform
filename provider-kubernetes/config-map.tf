dcc225de579bc155dc848a89f3862d8078be5674
Adama SEYE <a.seye3777@zig.univ.sn>
2025-01-12 17:19:13 +0100

    fix connect service mysql

--- provider-kubernetes/config-map.tf
+++ provider-kubernetes/config-map.tf
@@ -4,7 +4,7 @@ resource "kubernetes_config_map" "myapp-config" {
     namespace = kubernetes_namespace.demo_app_ns.metadata[0].name
   }
   data = {
-    mysql-host          = "mydb"
+    mysql-host          = "demo-app-service"
     mysql-username      = "adama"
     mysql-database-name = "db_product"
   }
