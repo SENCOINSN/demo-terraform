5b1a78c6050942831b9172ecaed5c62103b6f3a8
Adama SEYE <a.seye3777@zig.univ.sn>
2025-01-13 20:23:43 +0100

    unique true

--- src/main/java/com/sid/gl/Product.java
+++ src/main/java/com/sid/gl/Product.java
@@ -1,5 +1,6 @@
 package com.sid.gl;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
@@ -17,6 +18,7 @@ public class Product {
     @Id
     @GeneratedValue(strategy = jakarta.persistence.GenerationType.IDENTITY)
     private Long id;
+    @Column(unique = true)
     private String name;
     private String description;
     private double price;
