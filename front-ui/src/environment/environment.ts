e19ffd0c721ee034e8af55b6a77bf7cfe7431551
Adama SEYE <a.seye3777@zig.univ.sn>
2025-01-27 19:21:15 +0100

    k8s impl

--- front-ui/src/environment/environment.ts
+++ front-ui/src/environment/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    //api:"http://localhost:8087/api/v1"
-    api: "http://api.moodd.xyz/api/v1"
+    api:"http://localhost:8087/api/v1"
+    //api: "http://api.moodd.xyz/api/v1"
   };
